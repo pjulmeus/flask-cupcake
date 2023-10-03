f5dad0ea6aace51bb0ba8bde83923cc37749bc90
pjulmeus <pjulmeus@gmail.com>
2023-10-03 00:31:44 -0400

    first commit

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, Cupcake
+import requests
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcake_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+@app.route("/")
+def homepage():
+    return "SPURS"
